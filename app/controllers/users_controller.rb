b6a24812657d94119cc45532d8613f404596f132
yasirp0 <yasirp0@ChicagoBooth.edu>
2022-11-11 22:06:59 +0000

    user details look good

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,26 @@ class UsersController < ActionController::Base
 
   def index
     matching_users = User.all
-    
+
     @list_of_users =  matching_users.order({:username =>:asc})
 
     render ({ :template => "users_templates/index.html.erb"})
   end
 
+  def show
+
+    #Parameters: {"path_username"=>"anisa"}
+  url_username = params.fetch("path_username")
+
+  matching_usernames = User.where({ :username => url_username})
+
+  @the_user = matching_usernames.first
+
+  #if the_user == nil
+    #redirect_to("/404")
+  #else
+    render ({ :template => "users_templates/show.html.erb"})
+  #end
+
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get("/users", { :controller => "users",
   :action=> "index"})
 
+  get("/users/:path_username", { :controller => "users",
+  :action=> "show"})
 
 end
